aff6c116d82f1ecb02b45edc9305c104523fe155
SamNelson1998 <snelson@elevenwinds.com>
2014-09-23 07:54:08 -0400

    Eleven Winds - FluentMigrator
    Simply an interface for FluentMigrator.

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace FluentMigrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Run = new System.Windows.Forms.Button();
+            this.ServerBox = new System.Windows.Forms.TextBox();
+            this.Server = new System.Windows.Forms.Label();
+            this.Database = new System.Windows.Forms.Label();
+            this.DatabaseBox = new System.Windows.Forms.TextBox();
+            this.StoretypeBox = new System.Windows.Forms.ComboBox();
+            this.Storetype = new System.Windows.Forms.Label();
+            this.Environment = new System.Windows.Forms.Label();
+            this.EnvironmentBox = new System.Windows.Forms.ComboBox();
+            this.Databasetype = new System.Windows.Forms.Label();
+            this.DatabasetypeBox = new System.Windows.Forms.ComboBox();
+            this.Validation = new System.Windows.Forms.Label();
+            this.Validate = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rollback = new System.Windows.Forms.RadioButton();
+            this.migrate = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(375, 87);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 0;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // ServerBox
+            // 
+            this.ServerBox.Location = new System.Drawing.Point(9, 25);
+            this.ServerBox.Name = "ServerBox";
+            this.ServerBox.Size = new System.Drawing.Size(160, 20);
+            this.ServerBox.TabIndex = 1;
+            this.ServerBox.TextChanged += new System.EventHandler(this.ServerBox_TextChanged);
+            // 
+            // Server
+            // 
+            this.Server.AutoSize = true;
+            this.Server.BackColor = System.Drawing.Color.Transparent;
+            this.Server.Location = new System.Drawing.Point(9, 9);
+            this.Server.Name = "Server";
+            this.Server.Size = new System.Drawing.Size(41, 13);
+            this.Server.TabIndex = 2;
+            this.Server.Text = "Server:";
+            this.Server.Click += new System.EventHandler(this.Server_Click);
+            // 
+            // Database
+            // 
+            this.Database.AutoSize = true;
+            this.Database.BackColor = System.Drawing.Color.Transparent;
+            this.Database.Location = new System.Drawing.Point(9, 58);
+            this.Database.Name = "Database";
+            this.Database.Size = new System.Drawing.Size(56, 13);
+            this.Database.TabIndex = 3;
+            this.Database.Text = "Database:";
+            this.Database.Click += new System.EventHandler(this.Database_Click);
+            // 
+            // DatabaseBox
+            // 
+            this.DatabaseBox.Location = new System.Drawing.Point(9, 74);
+            this.DatabaseBox.Name = "DatabaseBox";
+            this.DatabaseBox.Size = new System.Drawing.Size(160, 20);
+            this.DatabaseBox.TabIndex = 4;
+            this.DatabaseBox.TextChanged += new System.EventHandler(this.DatabaseBox_TextChanged);
+            // 
+            // StoretypeBox
+            // 
+            this.StoretypeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.StoretypeBox.FormattingEnabled = true;
+            this.StoretypeBox.Location = new System.Drawing.Point(194, 25);
+            this.StoretypeBox.Name = "StoretypeBox";
+            this.StoretypeBox.Size = new System.Drawing.Size(157, 21);
+            this.StoretypeBox.TabIndex = 6;
+            this.StoretypeBox.SelectedIndexChanged += new System.EventHandler(this.StoretypeBox_SelectedIndexChanged);
+            // 
+            // Storetype
+            // 
+            this.Storetype.AutoSize = true;
+            this.Storetype.BackColor = System.Drawing.Color.Transparent;
+            this.Storetype.Location = new System.Drawing.Point(191, 9);
+            this.Storetype.Name = "Storetype";
+            this.Storetype.Size = new System.Drawing.Size(62, 13);
+            this.Storetype.TabIndex = 7;
+            this.Storetype.Text = "Store Type:";
+            // 
+            // Environment
+            // 
+            this.Environment.AutoSize = true;
+            this.Environment.BackColor = System.Drawing.Color.Transparent;
+            this.Environment.Location = new System.Drawing.Point(191, 58);
+            this.Environment.Name = "Environment";
+            this.Environment.Size = new System.Drawing.Size(69, 13);
+            this.Environment.TabIndex = 8;
+            this.Environment.Text = "Environment:";
+            // 
+            // EnvironmentBox
+            // 
+            this.EnvironmentBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EnvironmentBox.FormattingEnabled = true;
+            this.EnvironmentBox.Location = new System.Drawing.Point(194, 74);
+            this.EnvironmentBox.Name = "EnvironmentBox";
+            this.EnvironmentBox.Size = new System.Drawing.Size(157, 21);
+            this.EnvironmentBox.TabIndex = 9;
+            this.EnvironmentBox.SelectedIndexChanged += new System.EventHandler(this.EnvironmentBox_SelectedIndexChanged);
+            // 
+            // Databasetype
+            // 
+            this.Databasetype.AutoSize = true;
+            this.Databasetype.BackColor = System.Drawing.Color.Transparent;
+            this.Databasetype.Location = new System.Drawing.Point(191, 107);
+            this.Databasetype.Name = "Databasetype";
+            this.Databasetype.Size = new System.Drawing.Size(80, 13);
+            this.Databasetype.TabIndex = 10;
+            this.Databasetype.Text = "DatabaseType:";
+            // 
+            // DatabasetypeBox
+            // 
+            this.DatabasetypeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DatabasetypeBox.FormattingEnabled = true;
+            this.DatabasetypeBox.Location = new System.Drawing.Point(194, 123);
+            this.DatabasetypeBox.Name = "DatabasetypeBox";
+            this.DatabasetypeBox.Size = new System.Drawing.Size(157, 21);
+            this.DatabasetypeBox.TabIndex = 11;
+            this.DatabasetypeBox.SelectedIndexChanged += new System.EventHandler(this.DatabasetypeBox_SelectedIndexChanged);
+            // 
+            // Validation
+            // 
+            this.Validation.AutoSize = true;
+            this.Validation.Location = new System.Drawing.Point(15, 305);
+            this.Validation.Name = "Validation";
+            this.Validation.Size = new System.Drawing.Size(0, 13);
+            this.Validation.TabIndex = 12;
+            // 
+            // Validate
+            // 
+            this.Validate.AutoSize = true;
+            this.Validate.BackColor = System.Drawing.Color.Transparent;
+            this.Validate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Validate.ForeColor = System.Drawing.Color.Red;
+            this.Validate.Location = new System.Drawing.Point(15, 126);
+            this.Validate.Name = "Validate";
+            this.Validate.Size = new System.Drawing.Size(141, 13);
+            this.Validate.TabIndex = 13;
+            this.Validate.Text = "*Please fill in all parameters.*";
+            this.Validate.Visible = false;
+            this.Validate.Click += new System.EventHandler(this.Validate_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(375, 116);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 14;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.OutputBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.OutputBox.Location = new System.Drawing.Point(9, 178);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.Size = new System.Drawing.Size(441, 178);
+            this.OutputBox.TabIndex = 15;
+            this.OutputBox.TabStop = false;
+            this.OutputBox.Text = "";
+            this.OutputBox.TextChanged += new System.EventHandler(this.OutputBox_TextChanged);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.OutputLabel.Location = new System.Drawing.Point(12, 162);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(42, 13);
+            this.OutputLabel.TabIndex = 16;
+            this.OutputLabel.Text = "Output:";
+            this.OutputLabel.Click += new System.EventHandler(this.OutputLabel_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.rollback);
+            this.panel1.Controls.Add(this.migrate);
+            this.panel1.Location = new System.Drawing.Point(357, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(93, 71);
+            this.panel1.TabIndex = 17;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // rollback
+            // 
+            this.rollback.AutoSize = true;
+            this.rollback.Location = new System.Drawing.Point(18, 39);
+            this.rollback.Name = "rollback";
+            this.rollback.Size = new System.Drawing.Size(62, 17);
+            this.rollback.TabIndex = 1;
+            this.rollback.TabStop = true;
+            this.rollback.Text = "rollback";
+            this.rollback.UseVisualStyleBackColor = true;
+            this.rollback.CheckedChanged += new System.EventHandler(this.rollback_CheckedChanged);
+            // 
+            // migrate
+            // 
+            this.migrate.AutoSize = true;
+            this.migrate.Location = new System.Drawing.Point(18, 16);
+            this.migrate.Name = "migrate";
+            this.migrate.Size = new System.Drawing.Size(59, 17);
+            this.migrate.TabIndex = 0;
+            this.migrate.TabStop = true;
+            this.migrate.Text = "migrate";
+            this.migrate.UseVisualStyleBackColor = true;
+            this.migrate.CheckedChanged += new System.EventHandler(this.migrate_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackgroundImage = global::FluentMigrator.Properties.Resources.Background3;
+            this.ClientSize = new System.Drawing.Size(462, 367);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Validate);
+            this.Controls.Add(this.Validation);
+            this.Controls.Add(this.DatabasetypeBox);
+            this.Controls.Add(this.Databasetype);
+            this.Controls.Add(this.EnvironmentBox);
+            this.Controls.Add(this.Environment);
+            this.Controls.Add(this.Storetype);
+            this.Controls.Add(this.StoretypeBox);
+            this.Controls.Add(this.DatabaseBox);
+            this.Controls.Add(this.Database);
+            this.Controls.Add(this.Server);
+            this.Controls.Add(this.ServerBox);
+            this.Controls.Add(this.Run);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FluentMigrator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.TextBox ServerBox;
+        private System.Windows.Forms.Label Server;
+        private System.Windows.Forms.Label Database;
+        private System.Windows.Forms.TextBox DatabaseBox;
+        private System.Windows.Forms.ComboBox StoretypeBox;
+        private System.Windows.Forms.Label Storetype;
+        private System.Windows.Forms.Label Environment;
+        private System.Windows.Forms.ComboBox EnvironmentBox;
+        private System.Windows.Forms.Label Databasetype;
+        private System.Windows.Forms.ComboBox DatabasetypeBox;
+        private System.Windows.Forms.Label Validation;
+        private System.Windows.Forms.Label Validate;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rollback;
+        private System.Windows.Forms.RadioButton migrate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FluentMigrator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StoretypeBox.Items.Add("Trees");
+            StoretypeBox.Items.Add("Pools");
+            StoretypeBox.Items.Add("International");
+
+            EnvironmentBox.Items.Add("Production");
+            EnvironmentBox.Items.Add("Staging");
+            EnvironmentBox.Items.Add("Development");
+
+            DatabasetypeBox.Items.Add("Main");
+            DatabasetypeBox.Items.Add("History");
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            if (ServerBox.Text == String.Empty
+                || DatabaseBox.Text == String.Empty
+                || StoretypeBox.Text == String.Empty
+                || EnvironmentBox.Text == String.Empty
+                || DatabasetypeBox.Text == String.Empty
+                || (rollback.Checked == false && migrate.Checked == false))
+            {
+                Validate.Visible = true;
+            }
+            else
+            {
+                Validate.Visible = false;
+                string store = StoretypeBox.SelectedItem.ToString();
+                string environment = EnvironmentBox.SelectedItem.ToString();
+                string datatype = DatabasetypeBox.SelectedItem.ToString();
+                string cast = "";
+
+                if (migrate.Checked)
+                    cast = "migrate";
+                else
+                    cast = "rollback";
+
+                Process migraterun = new Process();
+                ProcessStartInfo StartInfo = new ProcessStartInfo();
+                StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                StartInfo.CreateNoWindow = true;
+                StartInfo.RedirectStandardOutput = true;
+                StartInfo.RedirectStandardError = true;
+                StartInfo.UseShellExecute = false;
+                StartInfo.FileName = "C:\\Code\\warehouse-manager-web\\packages\\FluentMigrator.1.3.0.0\\tools\\Migrate.exe";
+                StartInfo.Arguments = " /connection " + "\"Data Source=" + ServerBox.Text +
+                                      ";Initial Catalog=" + DatabaseBox.Text +
+                                      ";User ID=wmuser;Password='b@ls@m-h1ll;';\"" +
+                                      " /db sqlserver /target C:\\Code\\warehouse-manager-web\\Migrations\\bin\\Debug\\Migrations.dll /task " + cast
+                                      +" /context Development /tag " + store + " /tag " + environment + " /tag " + datatype;
+                migraterun.StartInfo = StartInfo;
+                migraterun.Start();
+
+                string output = "";
+
+                while (!migraterun.HasExited)
+                {
+                    output += migraterun.StandardOutput.ReadToEnd() + migraterun.StandardError.ReadToEnd();
+                    OutputBox.Text = output;
+                }
+            }
+        }
+
+        private void ServerBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Server_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StoretypeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DatabaseBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnvironmentBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DatabasetypeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Database_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Validate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OutputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void migrate_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rollback_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
